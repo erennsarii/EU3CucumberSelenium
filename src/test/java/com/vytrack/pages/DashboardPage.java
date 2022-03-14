db4598acf9388ea89274184909e090aa3d2a612d
erennsarii <erennsarii@yandex.com>
2022-03-14 19:47:20 +0300

    EU3 Cucumber Selenium started.

---
+++ src/test/java/com/vytrack/pages/DashboardPage.java
@@ -0,0 +1,9 @@
+package com.vytrack.pages;
+
+public class DashboardPage extends BasePage{
+
+    public DashboardPage() {
+
+        //no need to explicitly write constructor,because it will use its parents  constructor
+    }
+}
